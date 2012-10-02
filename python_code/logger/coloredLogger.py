094543a9598238fd130ed326e7c9237d4f3ad463
unknown <b03418@099w6027.bh.com.ar>
2012-10-02 16:40:40 -0300

    Added logger/CouchDB wrapper/ConfigParser

---
+++ python_code/logger/coloredLogger.py
@@ -0,0 +1,27 @@
+import logging
+import coloredFormater as c
+from abc import ABCMeta
+
+class AbstractLogger(logging.Logger):
+
+    __metaclass__=ABCMeta
+
+    def __init__(self,name):
+        raise NotImplementedError
+
+
+# Custom logger class with multiple destinations
+class ColoredLogger(AbstractLogger):
+    FORMAT = "[$BOLD%(name)-s$RESET][%(levelname)-s]  %(message)s ($BOLD%(filename)s$RESET:%(lineno)d) at %(asctime)-15s"
+    COLOR_FORMAT = c.formatter_message(FORMAT, True)
+    def __init__(self, name):
+        logging.Logger.__init__(self, name, logging.DEBUG)
+
+        color_formatter = c.ColoredFormatter(self.COLOR_FORMAT)
+
+        console = logging.StreamHandler()
+        console.setFormatter(color_formatter)
+
+        self.addHandler(console)
+        logging.setLoggerClass(ColoredLogger)
+        return
